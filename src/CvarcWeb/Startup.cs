338bfa33ea8c1e0e3131eaf10db1cae1d9a3c987
FoKycHuK <fokychuk47@ya.ru>
2016-10-19 22:46:00 +0500

    пароль теперь требует только 6 символов и без всякой шняги

--- src/CvarcWeb/Startup.cs
+++ src/CvarcWeb/Startup.cs
@@ -52,6 +52,15 @@ namespace CvarcWeb
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
