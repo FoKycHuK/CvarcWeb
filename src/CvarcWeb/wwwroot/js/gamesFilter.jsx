50339617867fec13becabb7983a94e088cd26eec
Mironov Alexey <av_mironov@skbkontur.ru>
2016-12-04 15:32:17 +0500

    автокомплит для названия команды

--- src/CvarcWeb/wwwroot/js/gamesFilter.jsx
+++ src/CvarcWeb/wwwroot/js/gamesFilter.jsx
@@ -1,185 +1,27 @@
 ﻿import {Component} from 'react';
 import Games from './games';
+import TeamNameFilter from './teamNameFilter';
 
 class GamesFilter extends Component {
     constructor() {
         super();
         this.state = {};
         this.state.games = [];
-        var params = { GameName: "TestGame" };
-        var query = Object.keys(params)
-            .map(k => encodeURIComponent(k) + "=" + encodeURIComponent(params[k]))
-            .join("&");
-        fetch(`Games/Find?${query}`, {
+        fetch(`Games/Find${document.location.search}`, {
                     credentials: "same-origin",
                     headers: { "Content-Type": "application/json", "enctype": "json" }
                 }
-            )
-            .then(data => data.json())
-            .then(data => this.setState({ games: data.games }));
+        ).then(data => data.json())
+         .then(data => this.setState({ games: data.games }));
     }
 
     render() {
-        var games = [
-            {
-                "GameId": 1,
-                "GameName": "TestGame",
-                "PathToLog": "C:/",
-                "TeamGameResults": [
-                  {
-                      "TeamGameResultId": 1,
-                      "Team": {
-                          "TeamId": 1,
-                          "Name": "Pizdec Kakoe Dlinnoe Imya Komandy",
-                          "Owner": null,
-                          "CvarcTag": "123",
-                          "LinkToImage": "qwe",
-                          "Members": null
-                      },
-                      "Results": [
-                        {
-                            "ResultId": 1,
-                            "Scores": 10,
-                            "ScoresType": "MainScores"
-                        },
-                        {
-                            "ResultId": 2,
-                            "Scores": 20,
-                            "ScoresType": "OtherScores"
-                        }
-                      ]
-                  }
-                ]
-            },
-            {
-                "GameId": 2,
-                "GameName": "Second Game",
-                "PathToLog": "C:/",
-                "TeamGameResults": [
-                  {
-                      "TeamGameResultId": 3,
-                      "Team": {
-                          "TeamId": 3,
-                          "Name": "Pizdec Kakoe Dlinnoe Imya Komandy",
-                          "Owner": null,
-                          "CvarcTag": "123",
-                          "LinkToImage": "qwe",
-                          "Members": null
-                      },
-                      "Results": [
-                        {
-                            "ResultId": 5,
-                            "Scores": 10,
-                            "ScoresType": "MainScores"
-                        },
-                        {
-                            "ResultId": 6,
-                            "Scores": 20,
-                            "ScoresType": "OtherScores"
-                        }
-                      ]
-                  },
-                  {
-                      "TeamGameResultId": 4,
-                      "Team": {
-                          "TeamId": 4,
-                          "Name": "Bleat' Pizdec Kakoe Dlinnoe Imya Komandy",
-                          "Owner": null,
-                          "CvarcTag": "1234",
-                          "LinkToImage": "qwer",
-                          "Members": null
-                      },
-                      "Results": [
-                        {
-                            "ResultId": 7,
-                            "Scores": 50,
-                            "ScoresType": "MainScores"
-                        },
-                        {
-                            "ResultId": 8,
-                            "Scores": 70,
-                            "ScoresType": "OtherScores"
-                        }
-                      ]
-                  }
-                ]
-            },
-            {
-                "GameId": 3,
-                "GameName": "TestGame",
-                "PathToLog": "C:/",
-                "TeamGameResults": [
-                    {
-                        "TeamGameResultId": 3,
-                        "Team": {
-                            "TeamId": 1,
-                            "Name": "Winners",
-                            "Owner": null,
-                            "CvarcTag": "123",
-                            "LinkToImage": "qwe",
-                            "Members": null
-                        },
-                        "Results": [
-                        {
-                            "ResultId": 1,
-                            "Scores": 10,
-                            "ScoresType": "MainScores"
-                        },
-                        {
-                            "ResultId": 2,
-                            "Scores": 20,
-                            "ScoresType": "OtherScores"
-                        }
-                        ]
-                    },
-                    {
-                        "TeamGameResultId": 2,
-                        "Team": {
-                            "TeamId": 2,
-                            "Name": "Loosers",
-                            "Owner": null,
-                            "CvarcTag": "1234",
-                            "LinkToImage": "qwer",
-                            "Members": null
-                        },
-                        "Results": [
-                        {
-                            "ResultId": 3,
-                            "Scores": 5,
-                            "ScoresType": "MainScores"
-                        },
-                        {
-                            "ResultId": 4,
-                            "Scores": 7,
-                            "ScoresType": "OtherScores"
-                        }
-                        ]
-                    }
-                ]
-            }
-        ];
-        //games = [];
-        var i = 4;
-        //var moreGames = games.map(g => {
-        //    var game = JSON.parse(JSON.stringify(g));
-        //    game.GameId = i++;
-        //    return game;
-        //});
-        //games = games.concat(moreGames);
-        //var moreGames = games.map(g => {
-        //    var game = JSON.parse(JSON.stringify(g));
-        //    game.GameId = i++;
-        //    return game;
-        //});
-        //games = games.concat(moreGames);
-        var moreGames = games.map(g => {
-            var game = JSON.parse(JSON.stringify(g));
-            game.GameId = i++;
-            return game;
-        });
-        games = games.concat(moreGames);
-
-        return (<Games games={this.state.games}/>);
+        return (
+            <div>
+                <TeamNameFilter/>
+                <Games games={this.state.games}/>
+            </div>
+        );
     }
 };
 
---
+++ src/CvarcWeb/wwwroot/js/teamNameFilter.jsx
@@ -0,0 +1,54 @@
+﻿import {Component} from 'react';
+import Rx from 'Rx';
+
+class TeamNameFilter extends Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        var teams = new Bloodhound({
+            datumTokenizer: function (datum) {
+                return Bloodhound.tokenizers.whitespace(datum.value);
+            },
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            remote: {
+                wildcard: "%QUERY",
+                url: "/Teams/?teamNamePrefix=%QUERY",
+                transform: function(response) {
+                    return $.map(response.teams, function(team) {
+                        return {
+                            value: team
+                        };
+                    });
+                }
+            }
+        });
+        $(this.__input).typeahead({
+            highlight: true,
+            minLength: 2
+        }, {
+            display: 'value',
+            source: teams
+        });
+    }
+
+    render() {
+        return (
+            <div className="team-name-filter">
+                <div className="row-fluid">
+                    <form role="form">
+                    <div className="form-group">
+                        <input type="text" className="typeahead form-control" placeholder="Team name" autoComplete="off" ref={(c) => this.__input = c}/>
+                    </div>
+                    </form>
+                </div>
+                <div className="row-fluid">
+                    <ul id="results" ref={(c) => this.__results = c}></ul>
+                </div>
+            </div>);
+    }
+};
+
+export default TeamNameFilter;
