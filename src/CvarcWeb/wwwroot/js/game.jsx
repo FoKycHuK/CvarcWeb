95a1a0e6f6663fe97216a22af6c3bb67f9086dcb
Mironov Alexey <av_mironov@skbkontur.ru>
2016-12-04 18:34:23 +0500

    Немного порефакторил, определяю ничью

--- src/CvarcWeb/wwwroot/js/game.jsx
+++ src/CvarcWeb/wwwroot/js/game.jsx
@@ -1,15 +1,44 @@
 ﻿import {Component} from 'react';
 
 class Game extends Component {
+    compareResults(res1, res2) {
+        if (this.getMainScore(res1) !==  this.getMainScore(res2)) {
+            return this.getMainScore(res1) - this.getMainScore(res2);
+        }
+        return this.sumOtherScores(res1) - this.sumOtherScores(res2);
+    }
+    
+    isMainScore(res) {
+        return res.ScoresType === "MainScores";
+    }
+
+    sumOtherScores(res) {
+        return res.Results.filter(r => !this.isMainScore(r)).reduce((sum, cur) => sum + cur.Scores, 0);
+    }
+
+    getMainScore(teamResults) {
+        return teamResults.Results.filter(this.isMainScore)[0].Scores;
+    }
+
+    getTeamClasses(game, i) {
+        const sortedResults = JSON.parse(JSON.stringify(game.TeamGameResults)).sort((a, b) => this.compareResults(a, b));
+        const isPvp = game.TeamGameResults.length > 1;
+        const isWinner = () => this.compareResults(sortedResults[1], game.TeamGameResults[i]) === 0 &&
+                       this.compareResults(sortedResults[0], game.TeamGameResults[i]) !== 0;
+        const isDraw = () => this.compareResults(sortedResults[0], sortedResults[1]) === 0;
+        return isPvp && !isDraw()
+                    ? isWinner() 
+                        ? "team-result winner" 
+                        : "team-result looser" 
+                    : "team-result";
+
+    }
+
     render() {
         const game = this.props;
         const results = game.TeamGameResults.map((r, i) => {
-            var isMainScores = (scoreType) => scoreType === "MainScores";
-            var allMainScores = game.TeamGameResults.map(teamRes => teamRes.Results.filter(res => isMainScores(res.ScoresType))[0].Scores);
-            var isPvp = game.TeamGameResults.length > 1;
-            var isWinner = isPvp && allMainScores.filter(s => s < allMainScores[i]).length === allMainScores.length - 1;
-            var classes = isWinner ? "team-result winner" : isPvp ? "team-result looser" : "team-result";
-            return <div className={classes} key={r.Results.map(res => res.ResultId).join("/")}>
+            const classes = this.getTeamClasses(game, i);
+            return <div className={classes} key={r.TeamGameResultId}>
                        <div className="team-name">{r.Team.Name}</div>
                        {r.Results.map(res =>
                            <div className="scores" key={res.ResultId}>
